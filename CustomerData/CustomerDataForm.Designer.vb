2164aa62c761e66cc0d67734f9f13b9d45e2857a
tim <rosstimo@isu.edu>
2023-03-09 08:26:37 -0700

    name controls

--- CustomerData/CustomerDataForm.Designer.vb
+++ CustomerData/CustomerDataForm.Designer.vb
@@ -26,10 +26,10 @@ Partial Class CustomerDataForm
         Me.GroupBox2 = New System.Windows.Forms.GroupBox()
         Me.GroupBox3 = New System.Windows.Forms.GroupBox()
         Me.ButtonGroupBox = New System.Windows.Forms.GroupBox()
-        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.DisplayListBox = New System.Windows.Forms.ListBox()
         Me.ExitButton = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SubmitButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
         Me.GroupBox3.SuspendLayout()
         Me.ButtonGroupBox.SuspendLayout()
         Me.SuspendLayout()
@@ -54,7 +54,7 @@ Partial Class CustomerDataForm
         '
         'GroupBox3
         '
-        Me.GroupBox3.Controls.Add(Me.ListBox1)
+        Me.GroupBox3.Controls.Add(Me.DisplayListBox)
         Me.GroupBox3.Location = New System.Drawing.Point(395, 152)
         Me.GroupBox3.Name = "GroupBox3"
         Me.GroupBox3.Size = New System.Drawing.Size(633, 324)
@@ -63,8 +63,8 @@ Partial Class CustomerDataForm
         '
         'ButtonGroupBox
         '
-        Me.ButtonGroupBox.Controls.Add(Me.Button3)
-        Me.ButtonGroupBox.Controls.Add(Me.Button2)
+        Me.ButtonGroupBox.Controls.Add(Me.ClearButton)
+        Me.ButtonGroupBox.Controls.Add(Me.SubmitButton)
         Me.ButtonGroupBox.Controls.Add(Me.ExitButton)
         Me.ButtonGroupBox.Location = New System.Drawing.Point(27, 482)
         Me.ButtonGroupBox.Name = "ButtonGroupBox"
@@ -72,14 +72,14 @@ Partial Class CustomerDataForm
         Me.ButtonGroupBox.TabIndex = 3
         Me.ButtonGroupBox.TabStop = False
         '
-        'ListBox1
+        'DisplayListBox
         '
-        Me.ListBox1.FormattingEnabled = True
-        Me.ListBox1.ItemHeight = 25
-        Me.ListBox1.Location = New System.Drawing.Point(6, 30)
-        Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(621, 279)
-        Me.ListBox1.TabIndex = 0
+        Me.DisplayListBox.FormattingEnabled = True
+        Me.DisplayListBox.ItemHeight = 25
+        Me.DisplayListBox.Location = New System.Drawing.Point(6, 30)
+        Me.DisplayListBox.Name = "DisplayListBox"
+        Me.DisplayListBox.Size = New System.Drawing.Size(621, 279)
+        Me.DisplayListBox.TabIndex = 0
         '
         'ExitButton
         '
@@ -90,23 +90,23 @@ Partial Class CustomerDataForm
         Me.ExitButton.Text = "E&xit"
         Me.ExitButton.UseVisualStyleBackColor = True
         '
-        'Button2
+        'SubmitButton
         '
-        Me.Button2.Location = New System.Drawing.Point(533, 30)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(150, 114)
-        Me.Button2.TabIndex = 1
-        Me.Button2.Text = "Button2"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.SubmitButton.Location = New System.Drawing.Point(533, 30)
+        Me.SubmitButton.Name = "SubmitButton"
+        Me.SubmitButton.Size = New System.Drawing.Size(150, 114)
+        Me.SubmitButton.TabIndex = 1
+        Me.SubmitButton.Text = "&Submit"
+        Me.SubmitButton.UseVisualStyleBackColor = True
         '
-        'Button3
+        'ClearButton
         '
-        Me.Button3.Location = New System.Drawing.Point(689, 30)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(150, 114)
-        Me.Button3.TabIndex = 2
-        Me.Button3.Text = "Button3"
-        Me.Button3.UseVisualStyleBackColor = True
+        Me.ClearButton.Location = New System.Drawing.Point(689, 30)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(150, 114)
+        Me.ClearButton.TabIndex = 2
+        Me.ClearButton.Text = "&Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
         '
         'CustomerDataForm
         '
@@ -128,9 +128,9 @@ Partial Class CustomerDataForm
     Friend WithEvents GroupBox1 As GroupBox
     Friend WithEvents GroupBox2 As GroupBox
     Friend WithEvents GroupBox3 As GroupBox
-    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents DisplayListBox As ListBox
     Friend WithEvents ButtonGroupBox As GroupBox
-    Friend WithEvents Button3 As Button
-    Friend WithEvents Button2 As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents SubmitButton As Button
     Friend WithEvents ExitButton As Button
 End Class
