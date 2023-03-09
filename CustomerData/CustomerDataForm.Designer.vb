f60e42bf4125aa02ca559e5ad9868e03a295055e
tim <rosstimo@isu.edu>
2023-03-09 08:40:46 -0700

    add test data

--- CustomerData/CustomerDataForm.Designer.vb
+++ CustomerData/CustomerDataForm.Designer.vb
@@ -25,11 +25,12 @@ Partial Class CustomerDataForm
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
         Me.GroupBox2 = New System.Windows.Forms.GroupBox()
         Me.GroupBox3 = New System.Windows.Forms.GroupBox()
-        Me.ButtonGroupBox = New System.Windows.Forms.GroupBox()
         Me.DisplayListBox = New System.Windows.Forms.ListBox()
-        Me.ExitButton = New System.Windows.Forms.Button()
-        Me.SubmitButton = New System.Windows.Forms.Button()
+        Me.ButtonGroupBox = New System.Windows.Forms.GroupBox()
         Me.ClearButton = New System.Windows.Forms.Button()
+        Me.SubmitButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
         Me.GroupBox3.SuspendLayout()
         Me.ButtonGroupBox.SuspendLayout()
         Me.SuspendLayout()
@@ -47,7 +48,7 @@ Partial Class CustomerDataForm
         '
         Me.GroupBox2.Location = New System.Drawing.Point(395, 35)
         Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Size = New System.Drawing.Size(633, 111)
+        Me.GroupBox2.Size = New System.Drawing.Size(1176, 111)
         Me.GroupBox2.TabIndex = 1
         Me.GroupBox2.TabStop = False
         Me.GroupBox2.Text = "Filter"
@@ -57,9 +58,19 @@ Partial Class CustomerDataForm
         Me.GroupBox3.Controls.Add(Me.DisplayListBox)
         Me.GroupBox3.Location = New System.Drawing.Point(395, 152)
         Me.GroupBox3.Name = "GroupBox3"
-        Me.GroupBox3.Size = New System.Drawing.Size(633, 324)
+        Me.GroupBox3.Size = New System.Drawing.Size(1176, 324)
         Me.GroupBox3.TabIndex = 2
         Me.GroupBox3.TabStop = False
+        '
+        'DisplayListBox
+        '
+        Me.DisplayListBox.Font = New System.Drawing.Font("Consolas", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.DisplayListBox.FormattingEnabled = True
+        Me.DisplayListBox.ItemHeight = 22
+        Me.DisplayListBox.Location = New System.Drawing.Point(6, 30)
+        Me.DisplayListBox.Name = "DisplayListBox"
+        Me.DisplayListBox.Size = New System.Drawing.Size(1164, 268)
+        Me.DisplayListBox.TabIndex = 0
         '
         'ButtonGroupBox
         '
@@ -68,60 +79,63 @@ Partial Class CustomerDataForm
         Me.ButtonGroupBox.Controls.Add(Me.ExitButton)
         Me.ButtonGroupBox.Location = New System.Drawing.Point(27, 482)
         Me.ButtonGroupBox.Name = "ButtonGroupBox"
-        Me.ButtonGroupBox.Size = New System.Drawing.Size(1001, 150)
+        Me.ButtonGroupBox.Size = New System.Drawing.Size(1544, 164)
         Me.ButtonGroupBox.TabIndex = 3
         Me.ButtonGroupBox.TabStop = False
         '
-        'DisplayListBox
-        '
-        Me.DisplayListBox.FormattingEnabled = True
-        Me.DisplayListBox.ItemHeight = 25
-        Me.DisplayListBox.Location = New System.Drawing.Point(6, 30)
-        Me.DisplayListBox.Name = "DisplayListBox"
-        Me.DisplayListBox.Size = New System.Drawing.Size(621, 279)
-        Me.DisplayListBox.TabIndex = 0
-        '
-        'ExitButton
-        '
-        Me.ExitButton.Location = New System.Drawing.Point(845, 30)
-        Me.ExitButton.Name = "ExitButton"
-        Me.ExitButton.Size = New System.Drawing.Size(150, 114)
-        Me.ExitButton.TabIndex = 0
-        Me.ExitButton.Text = "E&xit"
-        Me.ExitButton.UseVisualStyleBackColor = True
-        '
-        'SubmitButton
-        '
-        Me.SubmitButton.Location = New System.Drawing.Point(533, 30)
-        Me.SubmitButton.Name = "SubmitButton"
-        Me.SubmitButton.Size = New System.Drawing.Size(150, 114)
-        Me.SubmitButton.TabIndex = 1
-        Me.SubmitButton.Text = "&Submit"
-        Me.SubmitButton.UseVisualStyleBackColor = True
-        '
         'ClearButton
         '
-        Me.ClearButton.Location = New System.Drawing.Point(689, 30)
+        Me.ClearButton.Location = New System.Drawing.Point(1220, 30)
         Me.ClearButton.Name = "ClearButton"
         Me.ClearButton.Size = New System.Drawing.Size(150, 114)
         Me.ClearButton.TabIndex = 2
         Me.ClearButton.Text = "&Clear"
         Me.ClearButton.UseVisualStyleBackColor = True
         '
+        'SubmitButton
+        '
+        Me.SubmitButton.Location = New System.Drawing.Point(1064, 30)
+        Me.SubmitButton.Name = "SubmitButton"
+        Me.SubmitButton.Size = New System.Drawing.Size(150, 114)
+        Me.SubmitButton.TabIndex = 1
+        Me.SubmitButton.Text = "&Submit"
+        Me.SubmitButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(1376, 30)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(150, 114)
+        Me.ExitButton.TabIndex = 0
+        Me.ExitButton.Text = "E&xit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(24, 24)
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(1583, 32)
+        Me.MenuStrip1.TabIndex = 4
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
         'CustomerDataForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1040, 658)
+        Me.ClientSize = New System.Drawing.Size(1583, 658)
         Me.Controls.Add(Me.ButtonGroupBox)
         Me.Controls.Add(Me.GroupBox3)
         Me.Controls.Add(Me.GroupBox2)
         Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "CustomerDataForm"
         Me.Text = "Customer Data"
         Me.GroupBox3.ResumeLayout(False)
         Me.ButtonGroupBox.ResumeLayout(False)
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
@@ -133,4 +147,5 @@ Partial Class CustomerDataForm
     Friend WithEvents ClearButton As Button
     Friend WithEvents SubmitButton As Button
     Friend WithEvents ExitButton As Button
+    Friend WithEvents MenuStrip1 As MenuStrip
 End Class
--- CustomerData/CustomerDataForm.vb
+++ CustomerData/CustomerDataForm.vb
@@ -1,3 +1,21 @@
 ﻿Public Class CustomerDataForm
+    Private Sub SubmitButton_Click(sender As Object, e As EventArgs) Handles SubmitButton.Click
+        ShowTestData()
+    End Sub
 
+
+    Sub ShowTestData()
+        Dim columnData As String
+        Dim rowData As String
+
+        For row = 1 To 12
+            For column = 1 To 12
+                columnData = CStr(row * column) & " |"
+                rowData &= columnData.PadLeft(6)
+            Next
+            DisplayListBox.Items.Add(rowData)
+            rowData = ""
+        Next
+
+    End Sub
 End Class
