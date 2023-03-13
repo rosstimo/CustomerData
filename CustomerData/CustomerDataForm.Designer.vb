b89c162bd34b1ea2c32860c28625f0c0af8cafa3
tim <rosstimo@isu.edu>
2023-03-13 08:33:33 -0600

    add list box event exampl

--- CustomerData/CustomerDataForm.Designer.vb
+++ CustomerData/CustomerDataForm.Designer.vb
@@ -23,7 +23,13 @@ Partial Class CustomerDataForm
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.NameTextBox = New System.Windows.Forms.TextBox()
+        Me.NameLabel = New System.Windows.Forms.Label()
         Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.SearchLabel = New System.Windows.Forms.Label()
+        Me.SearchTextBox = New System.Windows.Forms.TextBox()
+        Me.CategoryLabel = New System.Windows.Forms.Label()
+        Me.CategoryComboBox = New System.Windows.Forms.ComboBox()
         Me.GroupBox3 = New System.Windows.Forms.GroupBox()
         Me.DisplayListBox = New System.Windows.Forms.ListBox()
         Me.ButtonGroupBox = New System.Windows.Forms.GroupBox()
@@ -31,12 +37,16 @@ Partial Class CustomerDataForm
         Me.SubmitButton = New System.Windows.Forms.Button()
         Me.ExitButton = New System.Windows.Forms.Button()
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
         Me.GroupBox3.SuspendLayout()
         Me.ButtonGroupBox.SuspendLayout()
         Me.SuspendLayout()
         '
         'GroupBox1
         '
+        Me.GroupBox1.Controls.Add(Me.NameTextBox)
+        Me.GroupBox1.Controls.Add(Me.NameLabel)
         Me.GroupBox1.Location = New System.Drawing.Point(27, 35)
         Me.GroupBox1.Name = "GroupBox1"
         Me.GroupBox1.Size = New System.Drawing.Size(362, 441)
@@ -44,21 +54,74 @@ Partial Class CustomerDataForm
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Costomer Information"
         '
+        'NameTextBox
+        '
+        Me.NameTextBox.Location = New System.Drawing.Point(84, 47)
+        Me.NameTextBox.Name = "NameTextBox"
+        Me.NameTextBox.Size = New System.Drawing.Size(258, 31)
+        Me.NameTextBox.TabIndex = 1
+        '
+        'NameLabel
+        '
+        Me.NameLabel.AutoSize = True
+        Me.NameLabel.Location = New System.Drawing.Point(15, 47)
+        Me.NameLabel.Name = "NameLabel"
+        Me.NameLabel.Size = New System.Drawing.Size(59, 25)
+        Me.NameLabel.TabIndex = 0
+        Me.NameLabel.Text = "Name"
+        '
         'GroupBox2
         '
+        Me.GroupBox2.Controls.Add(Me.SearchLabel)
+        Me.GroupBox2.Controls.Add(Me.SearchTextBox)
+        Me.GroupBox2.Controls.Add(Me.CategoryLabel)
+        Me.GroupBox2.Controls.Add(Me.CategoryComboBox)
         Me.GroupBox2.Location = New System.Drawing.Point(395, 35)
         Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Size = New System.Drawing.Size(1176, 111)
+        Me.GroupBox2.Size = New System.Drawing.Size(596, 111)
         Me.GroupBox2.TabIndex = 1
         Me.GroupBox2.TabStop = False
         Me.GroupBox2.Text = "Filter"
+        '
+        'SearchLabel
+        '
+        Me.SearchLabel.AutoSize = True
+        Me.SearchLabel.Location = New System.Drawing.Point(58, 24)
+        Me.SearchLabel.Name = "SearchLabel"
+        Me.SearchLabel.Size = New System.Drawing.Size(64, 25)
+        Me.SearchLabel.TabIndex = 3
+        Me.SearchLabel.Text = "Search"
+        '
+        'SearchTextBox
+        '
+        Me.SearchTextBox.Location = New System.Drawing.Point(127, 21)
+        Me.SearchTextBox.Name = "SearchTextBox"
+        Me.SearchTextBox.Size = New System.Drawing.Size(263, 31)
+        Me.SearchTextBox.TabIndex = 2
+        '
+        'CategoryLabel
+        '
+        Me.CategoryLabel.AutoSize = True
+        Me.CategoryLabel.Location = New System.Drawing.Point(37, 70)
+        Me.CategoryLabel.Name = "CategoryLabel"
+        Me.CategoryLabel.Size = New System.Drawing.Size(84, 25)
+        Me.CategoryLabel.TabIndex = 1
+        Me.CategoryLabel.Text = "Category"
+        '
+        'CategoryComboBox
+        '
+        Me.CategoryComboBox.FormattingEnabled = True
+        Me.CategoryComboBox.Location = New System.Drawing.Point(127, 70)
+        Me.CategoryComboBox.Name = "CategoryComboBox"
+        Me.CategoryComboBox.Size = New System.Drawing.Size(263, 33)
+        Me.CategoryComboBox.TabIndex = 0
         '
         'GroupBox3
         '
         Me.GroupBox3.Controls.Add(Me.DisplayListBox)
         Me.GroupBox3.Location = New System.Drawing.Point(395, 152)
         Me.GroupBox3.Name = "GroupBox3"
-        Me.GroupBox3.Size = New System.Drawing.Size(1176, 324)
+        Me.GroupBox3.Size = New System.Drawing.Size(596, 324)
         Me.GroupBox3.TabIndex = 2
         Me.GroupBox3.TabStop = False
         '
@@ -69,7 +132,7 @@ Partial Class CustomerDataForm
         Me.DisplayListBox.ItemHeight = 22
         Me.DisplayListBox.Location = New System.Drawing.Point(6, 30)
         Me.DisplayListBox.Name = "DisplayListBox"
-        Me.DisplayListBox.Size = New System.Drawing.Size(1164, 268)
+        Me.DisplayListBox.Size = New System.Drawing.Size(582, 268)
         Me.DisplayListBox.TabIndex = 0
         '
         'ButtonGroupBox
@@ -79,13 +142,13 @@ Partial Class CustomerDataForm
         Me.ButtonGroupBox.Controls.Add(Me.ExitButton)
         Me.ButtonGroupBox.Location = New System.Drawing.Point(27, 482)
         Me.ButtonGroupBox.Name = "ButtonGroupBox"
-        Me.ButtonGroupBox.Size = New System.Drawing.Size(1544, 164)
+        Me.ButtonGroupBox.Size = New System.Drawing.Size(964, 164)
         Me.ButtonGroupBox.TabIndex = 3
         Me.ButtonGroupBox.TabStop = False
         '
         'ClearButton
         '
-        Me.ClearButton.Location = New System.Drawing.Point(1220, 30)
+        Me.ClearButton.Location = New System.Drawing.Point(162, 30)
         Me.ClearButton.Name = "ClearButton"
         Me.ClearButton.Size = New System.Drawing.Size(150, 114)
         Me.ClearButton.TabIndex = 2
@@ -94,7 +157,7 @@ Partial Class CustomerDataForm
         '
         'SubmitButton
         '
-        Me.SubmitButton.Location = New System.Drawing.Point(1064, 30)
+        Me.SubmitButton.Location = New System.Drawing.Point(6, 30)
         Me.SubmitButton.Name = "SubmitButton"
         Me.SubmitButton.Size = New System.Drawing.Size(150, 114)
         Me.SubmitButton.TabIndex = 1
@@ -103,7 +166,7 @@ Partial Class CustomerDataForm
         '
         'ExitButton
         '
-        Me.ExitButton.Location = New System.Drawing.Point(1376, 30)
+        Me.ExitButton.Location = New System.Drawing.Point(318, 30)
         Me.ExitButton.Name = "ExitButton"
         Me.ExitButton.Size = New System.Drawing.Size(150, 114)
         Me.ExitButton.TabIndex = 0
@@ -115,23 +178,30 @@ Partial Class CustomerDataForm
         Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(24, 24)
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Size = New System.Drawing.Size(1583, 32)
+        Me.MenuStrip1.Size = New System.Drawing.Size(1010, 24)
         Me.MenuStrip1.TabIndex = 4
         Me.MenuStrip1.Text = "MenuStrip1"
         '
         'CustomerDataForm
         '
+        Me.AcceptButton = Me.SubmitButton
         Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1583, 658)
+        Me.ClientSize = New System.Drawing.Size(1010, 676)
         Me.Controls.Add(Me.ButtonGroupBox)
         Me.Controls.Add(Me.GroupBox3)
         Me.Controls.Add(Me.GroupBox2)
         Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.MenuStrip1)
         Me.MainMenuStrip = Me.MenuStrip1
+        Me.MaximumSize = New System.Drawing.Size(1920, 1080)
         Me.Name = "CustomerDataForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Customer Data"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
         Me.GroupBox3.ResumeLayout(False)
         Me.ButtonGroupBox.ResumeLayout(False)
         Me.ResumeLayout(False)
@@ -148,4 +218,10 @@ Partial Class CustomerDataForm
     Friend WithEvents SubmitButton As Button
     Friend WithEvents ExitButton As Button
     Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents CategoryComboBox As ComboBox
+    Friend WithEvents NameTextBox As TextBox
+    Friend WithEvents NameLabel As Label
+    Friend WithEvents SearchLabel As Label
+    Friend WithEvents SearchTextBox As TextBox
+    Friend WithEvents CategoryLabel As Label
 End Class
--- CustomerData/CustomerDataForm.vb
+++ CustomerData/CustomerDataForm.vb
@@ -1,8 +1,20 @@
-﻿Public Class CustomerDataForm
+﻿Option Strict On
+Option Explicit On
+
+Public Class CustomerDataForm
     Private Sub SubmitButton_Click(sender As Object, e As EventArgs) Handles SubmitButton.Click
-        ShowTestData()
+        'ShowTestData()
+        If NameTextBox.Text <> "" Then
+            DisplayListBox.Items.Add(NameTextBox.Text)
+        Else
+            MsgBox("Name is required")
+        End If
+        DefaultSetup()
     End Sub
 
+    Private Sub DefaultSetup()
+        NameTextBox.Text = ""
+    End Sub
 
     Sub ShowTestData()
         Dim columnData As String
@@ -17,5 +29,30 @@
             rowData = ""
         Next
 
+    End Sub
+
+
+    Private Sub CustomerDataForm_SizeChanged(sender As Object, e As EventArgs) Handles Me.SizeChanged
+        Me.Text = $"W:{Me.Width}, H:{Me.Height}"
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        DefaultSetup()
+    End Sub
+
+    Private Sub DisplayListBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles DisplayListBox.SelectedIndexChanged
+        NameTextBox.Text = DisplayListBox.SelectedItem.ToString
+    End Sub
+
+    Private Sub CustomerDataForm_Load(sender As Object, e As EventArgs) Handles Me.Load
+        DisplayListBox.Items.Add("Jimmy")
+        DisplayListBox.Items.Add("Frank")
+        DisplayListBox.Items.Add("Nancy")
+        DisplayListBox.Items.Add("Bill")
+        DisplayListBox.Items.Add("Sally")
     End Sub
 End Class
